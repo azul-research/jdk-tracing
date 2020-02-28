bd1dc901241cf8b45a2d577534f8c47d9be52b5d
Darya Razumova <d.razumova@yandex.ru>
2020-02-28 15:45:09 +0300

    fixed dtrace marco call

--- src/hotspot/share/utilities/hs_privateDtrace.h
+++ src/hotspot/share/utilities/hs_privateDtrace.h
@@ -1,18 +1,20 @@
-#define HS_PRIVATE_SAFEPOINT_BEGIN_WRAPPER (hs_private, safepoint__begin)\
-HS_PRIVATE_SAFEPOINT_BEGIN (hs_private, safepoint__begin)
+#include "utilities/dtrace.hpp";
 
-#define HS_PRIVATE_SAFEPOINT_END_WRAPPER (hs_private, safepoint__end)\
-HS_PRIVATE_SAFEPOINT_END (hs_private, safepoint__end)
+#define HS_PRIVATE_SAFEPOINT_BEGIN_WRAPPER (hs_private, safepoint__begin , )\
+HS_PRIVATE_SAFEPOINT_BEGIN ()
 
-#define HS_PRIVATE_CMS_INITMARK_BEGIN_WRAPPER (hs_private, cms__initmark__begin)\
-HS_PRIVATE_CMS_INITMARK_BEGIN (hs_private, cms__initmark__begin)
+#define HS_PRIVATE_SAFEPOINT_END_WRAPPER (hs_private, safepoint__end , )\
+HS_PRIVATE_SAFEPOINT_END ()
 
-#define HS_PRIVATE_CMS_INITMARK_END_WRAPPER (hs_private, cms__initmark__end)\
-HS_PRIVATE_CMS_INITMARK_END (hs_private, cms__initmark__end)
+#define HS_PRIVATE_CMS_INITMARK_BEGIN_WRAPPER (hs_private, cms__initmark__begin , )\
+HS_PRIVATE_CMS_INITMARK_BEGIN ()
 
-#define HS_PRIVATE_CMS_REMARK_BEGIN_WRAPPER (hs_private, cms__remark__begin)\
-HS_PRIVATE_CMS_REMARK_BEGIN (hs_private, cms__remark__begin)
+#define HS_PRIVATE_CMS_INITMARK_END_WRAPPER (hs_private, cms__initmark__end , )\
+HS_PRIVATE_CMS_INITMARK_END ()
 
-#define HS_PRIVATE_CMS_REMARK_END_WRAPPER (hs_private, cms__remark__end, arg0, arg1, arg2)\
-HS_PRIVATE_CMS_REMARK_END (hs_private, cms__remark__end, arg0, arg1, arg2)
+#define HS_PRIVATE_CMS_REMARK_BEGIN_WRAPPER (hs_private, cms__remark__begin , )\
+HS_PRIVATE_CMS_REMARK_BEGIN ()
+
+#define HS_PRIVATE_CMS_REMARK_END_WRAPPER (hs_private, cms__remark__end , arg0, arg1, arg2)\
+HS_PRIVATE_CMS_REMARK_END (arg0, arg1, arg2)
 
--- src/hotspot/share/utilities/hs_privateLTTngDtrace.h
+++ src/hotspot/share/utilities/hs_privateLTTngDtrace.h
@@ -1,20 +1,22 @@
+#include "utilities/dtrace.hpp";
+
 #include "utilities/lttng.hpp";
 
-#define HS_PRIVATE_SAFEPOINT_BEGIN_WRAPPER (hs_private, safepoint__begin)\
-HS_PRIVATE_SAFEPOINT_BEGIN (hs_private, safepoint__begin);tracepoint (hs_private, safepoint__begin)
+#define HS_PRIVATE_SAFEPOINT_BEGIN_WRAPPER (hs_private, safepoint__begin , )\
+HS_PRIVATE_SAFEPOINT_BEGIN ();tracepoint (hs_private, safepoint__begin , )
 
-#define HS_PRIVATE_SAFEPOINT_END_WRAPPER (hs_private, safepoint__end)\
-HS_PRIVATE_SAFEPOINT_END (hs_private, safepoint__end);tracepoint (hs_private, safepoint__end)
+#define HS_PRIVATE_SAFEPOINT_END_WRAPPER (hs_private, safepoint__end , )\
+HS_PRIVATE_SAFEPOINT_END ();tracepoint (hs_private, safepoint__end , )
 
-#define HS_PRIVATE_CMS_INITMARK_BEGIN_WRAPPER (hs_private, cms__initmark__begin)\
-HS_PRIVATE_CMS_INITMARK_BEGIN (hs_private, cms__initmark__begin);tracepoint (hs_private, cms__initmark__begin)
+#define HS_PRIVATE_CMS_INITMARK_BEGIN_WRAPPER (hs_private, cms__initmark__begin , )\
+HS_PRIVATE_CMS_INITMARK_BEGIN ();tracepoint (hs_private, cms__initmark__begin , )
 
-#define HS_PRIVATE_CMS_INITMARK_END_WRAPPER (hs_private, cms__initmark__end)\
-HS_PRIVATE_CMS_INITMARK_END (hs_private, cms__initmark__end);tracepoint (hs_private, cms__initmark__end)
+#define HS_PRIVATE_CMS_INITMARK_END_WRAPPER (hs_private, cms__initmark__end , )\
+HS_PRIVATE_CMS_INITMARK_END ();tracepoint (hs_private, cms__initmark__end , )
 
-#define HS_PRIVATE_CMS_REMARK_BEGIN_WRAPPER (hs_private, cms__remark__begin)\
-HS_PRIVATE_CMS_REMARK_BEGIN (hs_private, cms__remark__begin);tracepoint (hs_private, cms__remark__begin)
+#define HS_PRIVATE_CMS_REMARK_BEGIN_WRAPPER (hs_private, cms__remark__begin , )\
+HS_PRIVATE_CMS_REMARK_BEGIN ();tracepoint (hs_private, cms__remark__begin , )
 
-#define HS_PRIVATE_CMS_REMARK_END_WRAPPER (hs_private, cms__remark__end, arg0, arg1, arg2)\
-HS_PRIVATE_CMS_REMARK_END (hs_private, cms__remark__end, arg0, arg1, arg2);tracepoint (hs_private, cms__remark__end, arg0, arg1, arg2)
+#define HS_PRIVATE_CMS_REMARK_END_WRAPPER (hs_private, cms__remark__end , arg0, arg1, arg2)\
+HS_PRIVATE_CMS_REMARK_END (arg0, arg1, arg2);tracepoint (hs_private, cms__remark__end , arg0, arg1, arg2)
 
