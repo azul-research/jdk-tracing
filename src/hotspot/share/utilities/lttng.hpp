97f933c73017e51cabdde0ccb3588463f64a7231
Darya Razumova <d.razumova@yandex.ru>
2020-03-06 14:25:27 +0300

    lttng hotspot and hs_private tracepoints

---
+++ src/hotspot/share/utilities/lttng.hpp
@@ -0,0 +1,7 @@
+#ifndef SHARE_UTILITIES_LTTNG_HPP
+#define SHARE_UTILITIES_LTTNG_HPP
+
+#include <utilities/lttngTracepoints.hpp>
+#include <utilities/lttngHSPrivate.hpp>
+
+#endif // SHARE_UTILITIES_LTTNG_HPP
