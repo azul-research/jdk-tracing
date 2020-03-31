d6efb1e2f51b5bfeba4345cf27e73e30139e1321
Darya Razumova <d.razumova@yandex.ru>
2020-03-31 03:01:02 +0300

    dlopen example

--- dev-tracing/lttng_examples/dl/main.c
+++ dev-tracing/lttng_examples/dl/main.c
@@ -2,8 +2,15 @@
 #define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
 #include "tp.h"
 #include <stdio.h>
+#include <dlfcn.h>
 
 int main() {
-	tracepoint(provider_test, test, 1);
+    void* handle = dlopen("./libtpp.so", RTLD_NOW | RTLD_GLOBAL);
+    if (!handle) {
+        printf(":(\n");
+    } else {
+        printf(":)\n");    
+    }   
+    tracepoint(provider_test, test, 1);
 	printf("hello!!\n");
 }
