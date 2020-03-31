becd7fba8607e9c417b99d1c4e65ed29ecf0ca48
Darya Razumova <d.razumova@yandex.ru>
2020-03-31 04:14:56 +0300

    add arg names to hotspot_jni.d

--- dev-tracing/h_generator/run.sh
+++ dev-tracing/h_generator/run.sh
@@ -3,8 +3,8 @@ export CLASSPATH=".:/usr/local/lib/antlr-4.8-complete.jar:$CLASSPATH"
 
 touch $1/src/hotspot/share/utilities/hotspotLTTngDtrace.h
 touch $1/src/hotspot/share/utilities/hs_privateLTTngDtrace.h
-# touch $1/src/hotspot/share/utilities/hs_jniLTTngDtrace.h
+touch $1/src/hotspot/share/utilities/hs_jniLTTngDtrace.h
 
 java Main $1/src/hotspot/os/posix/dtrace/hotspot.d $1/src/hotspot/share/utilities/hotspotLTTngDtrace.h lttng dtrace
 java Main $1/src/hotspot/os/posix/dtrace/hs_private.d $1/src/hotspot/share/utilities/hs_privateLTTngDtrace.h lttng dtrace
-# java Main $1/src/hotspot/os/posix/dtrace/hotspot_jni.d $1/src/hotspot/share/utilities/hs_jniLTTngDtrace.h lttng dtrace
+java Main $1/src/hotspot/os/posix/dtrace/hotspot_jni.d $1/src/hotspot/share/utilities/hs_jniLTTngDtrace.h lttng dtrace
