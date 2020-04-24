8d4ee98e7a029b6197464c361207c2acdb112b73
Darya Razumova <d.razumova@yandex.ru>
2020-04-24 12:31:49 +0300

    fixes

--- src/hotspot/share/utilities/lttng.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SHARE_UTILITIES_LTTNG_HPP
-#define SHARE_UTILITIES_LTTNG_HPP
-
-#include <utilities/lttngTracepoints.hpp>
-#include <utilities/lttngHSPrivate.hpp>
-
-#endif // SHARE_UTILITIES_LTTNG_HPP
